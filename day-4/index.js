e43d03a96f74dd463fe4a2285d3b08903a689c72
Michał Walczuk <michal.walczuk@ext.denkwerk.com>
2023-12-04 18:00:04 +0100

    day 4: make code look cool 😎

--- day-4/index.js
+++ day-4/index.js
@@ -4,22 +4,20 @@ const { mapToNumArray } = require('../utils');
 const file = fs.readFileSync('input_1.txt', 'utf8').split('\n');
 const games = file.map((line) => parseGame(line));
 
-function getTotalPoints() {
+const getTotalPoints = () => {
     return games.reduce(
         (acc, { points }) => (points > 0 ? acc + Math.pow(2, points - 1) : acc),
         0
     );
 }
 
-function getTotalScratchcards() {
-    return games.reduce((acc, game) => acc + sumGame(game) + 1, 0);
-}
+const getTotalScratchcards = () =>
+    games.reduce((acc, game) => acc + sumGame(game) + 1, 0);
 
-function sumGame(game) {
-    return game.refs.reduce((acc, ref) => acc + sumGame(games[ref]) + 1, 0);
-}
+const sumGame = (game) =>
+    game.refs.reduce((acc, ref) => acc + sumGame(games[ref]) + 1, 0);
 
-function parseGame(string) {
+const parseGame = (string) => {
     const { gameId, left, right } =
         /Card\s+(?<gameId>\d+):\s+(?<left>[^|]+)\|\s*(?<right>.+)/.exec(string)
             ?.groups || {};
@@ -37,7 +35,9 @@ function parseGame(string) {
     };
 }
 
+
 console.log({
     part1Answer: getTotalPoints(),
     part2Answer: getTotalScratchcards()
 });
+
