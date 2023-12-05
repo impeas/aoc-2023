6272b33a1b5ff0124bda07e622e6b7adc6b99bea
Michał Walczuk <michal.walczuk@ext.denkwerk.com>
2023-12-05 11:53:40 +0100

    day 5: solve part 1

--- day-5/index.js
+++ day-5/index.js
@@ -2,46 +2,44 @@ const fs = require('fs');
 const { mapToNumArray } = require('../utils');
 
 const file = fs.readFileSync('input.txt', 'utf8').split('\n\n');
+const maps = file.slice(1);
+const parsedMaps = maps.map((map) => parseMap(map));
 
 const parseSeeds = (seedString) => {
     const { content } = /seeds: (?<content>.*)/.exec(seedString)?.groups || {};
-
-    return {
-        seeds: mapToNumArray(content)
-    };
+    return mapToNumArray(content);
 };
 
-const parseMap = (mapString) => {
+function parseMap(mapString) {
     const splitted = mapString.split('\n');
     const matrix = splitted.slice(1);
-
     const { from, to } =
         /(?<from>.*)-(.*)-(?<to>.* )/.exec(splitted[0])?.groups || {};
-
     return {
         from: from.trim(),
         to: to.trim(),
         matrix: matrix.map((row) => mapToNumArray(row))
     };
-};
+}
 
-const findLowestSeed = (map, seed) => {
+const findSeed = (map, seed) => {
     for (const row of map.matrix) {
         const [dest, start, length] = row;
-
         if (seed >= start && seed <= start + length - 1) {
-            const found = dest - start;
-
-            console.log('Mapping found', seed, '=>', seed + found);
-        } else {
+            const index = dest - start;
+            return seed + index;
         }
     }
+    return seed;
 };
 
 const seeds = parseSeeds(file[0]);
-const maps = file.slice(1);
-const parsedMaps = maps.map((map) => parseMap(map));
 
-findLowestSeed(parsedMaps[0], 99);
+const getLocationsForSeeds = () =>
+    seeds.map((seed) =>
+        parsedMaps.reduce((acc, map) => findSeed(map, acc), seed)
+    );
 
-findLowestSeed(parsedMaps[0], 97);
+console.log({
+    part1Answer: Math.min(...getLocationsForSeeds())
+});
