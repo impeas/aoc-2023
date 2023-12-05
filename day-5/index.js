f58664b58a0555079c18677ae6ab394fc06632be
Michał Walczuk <michal.walczuk@ext.denkwerk.com>
2023-12-05 20:22:02 +0100

    day 5: brute force the part 2

--- day-5/index.js
+++ day-5/index.js
@@ -1,16 +1,32 @@
 const fs = require('fs');
 const { mapToNumArray } = require('../utils');
 
+const chunkArray = (arr, size) =>
+    arr.length > size
+        ? [arr.slice(0, size), ...chunkArray(arr.slice(size), size)]
+        : [arr];
+
 const file = fs.readFileSync('input.txt', 'utf8').split('\n\n');
-const maps = file.slice(1);
-const parsedMaps = maps.map((map) => parseMap(map));
+const stringMaps = file.slice(1);
+const nMaps = stringMaps.map((map) => parseMap(map));
+const rMaps = stringMaps
+    .map((map) => parseMap(map))
+    .reverse()
+    .map((map) => {
+        return {
+            ...map,
+            matrix: map.matrix.map((row) => [row[1], row[0], row[2]])
+        };
+    });
 
 const parseSeeds = (seedString) => {
     const { content } = /seeds: (?<content>.*)/.exec(seedString)?.groups || {};
     return mapToNumArray(content);
 };
 
-function parseMap(mapString) {
+const seeds = parseSeeds(file[0]);
+
+function parseMap(mapString, reversed) {
     const splitted = mapString.split('\n');
     const matrix = splitted.slice(1);
     const { from, to } =
@@ -25,7 +41,7 @@ function parseMap(mapString) {
 const findSeed = (map, seed) => {
     for (const row of map.matrix) {
         const [dest, start, length] = row;
-        if (seed >= start && seed <= start + length - 1) {
+        if (seed >= start && seed < start + length) {
             const index = dest - start;
             return seed + index;
         }
@@ -33,13 +49,27 @@ const findSeed = (map, seed) => {
     return seed;
 };
 
-const seeds = parseSeeds(file[0]);
+function bruteForceSmallestLocation() {
+    for (let i = 1_000_000_00; i < 1_000_000_000; i++) {
+        const seedByLocation = rMaps.reduce(
+            (acc, map) => findSeed(map, acc),
+            i
+        );
+        for (const range of rangeSeeds) {
+            const [start, length] = range;
+            if (seedByLocation >= start && seedByLocation < start + length) {
+                return i;
+            }
+        }
+    }
+}
 
 const getLocationsForSeeds = () =>
-    seeds.map((seed) =>
-        parsedMaps.reduce((acc, map) => findSeed(map, acc), seed)
-    );
+    seeds.map((seed) => nMaps.reduce((acc, map) => findSeed(map, acc), seed));
+
+const rangeSeeds = chunkArray(seeds, 2);
 
 console.log({
-    part1Answer: Math.min(...getLocationsForSeeds())
+    part1Answer: Math.min(...getLocationsForSeeds()),
+    part2Answer: bruteForceSmallestLocation()
 });
