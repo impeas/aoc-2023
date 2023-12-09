28040ea5205f1d66f7955becdf604f7863c4ddb5
Michał Walczuk <michal.walczuk@ext.denkwerk.com>
2023-12-09 23:41:02 +0100

    day 9: refactor to one fn

--- day-9/index.js
+++ day-9/index.js
@@ -3,9 +3,8 @@ const sequences = readFile('input.txt').split('\n').map(mapToNumArray);
 
 const calcDiffs = (seq) => seq.slice(0, -1).reduce((acc, value, i) => [...acc, seq[i + 1] - value], []);
 const nextSeq = (diffs) => (diffs.some((diff) => diff !== 0) ? nextSeq(calcDiffs(diffs)) + diffs.at(-1) : 0);
-const prevSeq = (seq, diffs) => (diffs.some((diff) => diff !== 0) ? -1 * (prevSeq(diffs, calcDiffs(diffs)) - seq[0]) : -1 * -seq[0]);
 
 console.log(
     sequences.reduce((a, ss) => a + ss.at(-1) + nextSeq(calcDiffs(ss)), 0),
-    sequences.reduce((a, ss) => a + prevSeq(ss, calcDiffs(ss)), 0)
+    sequences.reduce((a, ss) => a + [...ss].reverse().at(-1) + nextSeq(calcDiffs([...ss].reverse())), 0)
 );
