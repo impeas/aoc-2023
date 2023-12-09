2eb737d2a5e188b56295489980be21706e4a4246
Michał Walczuk <michal.walczuk@ext.denkwerk.com>
2023-12-09 18:30:55 +0100

    day 9: reduce lines of code

--- day-9/index.js
+++ day-9/index.js
@@ -1,22 +1,17 @@
 const { readFile, mapToNumArray } = require('../utils');
-
 const sequences = readFile('input.txt').split('\n').map(mapToNumArray);
 
-function nextSeqValue(seq) {
+function nextSeq(seq) {
     const diffs = seq.slice(0, -1).reduce((acc, value, i) => [...acc, seq[i + 1] - value], []);
-    return diffs.some((diff) => diff !== 0) ? nextSeqValue(diffs) + diffs.at(-1) : 0;
+    return diffs.some((diff) => diff !== 0) ? nextSeq(diffs) + diffs.at(-1) : 0;
 }
 
-function prevSeqValue(seq) {
+function prevSeq(seq) {
     const diffs = seq.slice(0, -1).reduce((acc, value, i) => [...acc, seq[i + 1] - value], []);
-    return diffs.some((diff) => diff !== 0) ? -1 * (prevSeqValue(diffs) - seq[0]) : -1 * -seq[0];
+    return diffs.some((diff) => diff !== 0) ? -1 * (prevSeq(diffs) - seq[0]) : -1 * -seq[0];
 }
 
-const summedPrevSequences = sequences.reduce((acc, value) => acc + prevSeqValue(value), 0);
-
-const summedNextSequences = sequences.reduce((acc, seq) => acc + seq.at(-1) + nextSeqValue(seq), 0);
-
-console.log({
-    part1Answer: summedNextSequences,
-    part2Answer: summedPrevSequences
-});
+console.log(
+    sequences.reduce((a, ss) => a + ss.at(-1) + nextSeq(ss), 0),
+    sequences.reduce((a, ss) => a + prevSeq(ss), 0)
+);
