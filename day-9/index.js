8c676d751a1642eea435fc4846f8887bd7e670d9
Michał Walczuk <michal.walczuk@ext.denkwerk.com>
2023-12-09 20:18:35 +0100

    day 9: flex with one liners

--- day-9/index.js
+++ day-9/index.js
@@ -1,17 +1,11 @@
 const { readFile, mapToNumArray } = require('../utils');
 const sequences = readFile('input.txt').split('\n').map(mapToNumArray);
 
-function nextSeq(seq) {
-    const diffs = seq.slice(0, -1).reduce((acc, value, i) => [...acc, seq[i + 1] - value], []);
-    return diffs.some((diff) => diff !== 0) ? nextSeq(diffs) + diffs.at(-1) : 0;
-}
-
-function prevSeq(seq) {
-    const diffs = seq.slice(0, -1).reduce((acc, value, i) => [...acc, seq[i + 1] - value], []);
-    return diffs.some((diff) => diff !== 0) ? -1 * (prevSeq(diffs) - seq[0]) : -1 * -seq[0];
-}
+const calcDiffs = (seq) => seq.slice(0, -1).reduce((acc, value, i) => [...acc, seq[i + 1] - value], []);
+const nextSeq = (diffs) => (diffs.some((diff) => diff !== 0) ? nextSeq(calcDiffs(diffs)) + diffs.at(-1) : 0);
+const prevSeq = (seq, diffs) => (diffs.some((diff) => diff !== 0) ? -1 * (prevSeq(diffs, calcDiffs(diffs)) - seq[0]) : -1 * -seq[0]);
 
 console.log(
-    sequences.reduce((a, ss) => a + ss.at(-1) + nextSeq(ss), 0),
-    sequences.reduce((a, ss) => a + prevSeq(ss), 0)
+    sequences.reduce((a, ss) => a + ss.at(-1) + nextSeq(calcDiffs(ss)), 0),
+    sequences.reduce((a, ss) => a + prevSeq(ss, calcDiffs(ss)), 0)
 );
