c7edbc718af27fdeef343dd8ae9cc425066ab4a5
Michał Walczuk <michal.walczuk@ext.denkwerk.com>
2023-12-15 22:07:21 +0100

    day 11: solve part 2

--- day-11/index.ts
+++ day-11/index.ts
@@ -5,47 +5,58 @@ const galaxy = readFile('input.txt')
     .filter((x) => x)
     .map((x) => x.split(''));
 
-const expandedGalaxy = expandGalaxy(galaxy);
+const npos = getCalibratedPositions(galaxy, 1);
 
-const distances = findShortestDistancesBetweenGalaxies(expandedGalaxy);
+const shortest = findShortestDistancesBetweenGalaxies(npos);
 
-function findShortestDistancesBetweenGalaxies(galaxy) {
-    const galaxyPositions = galaxy.reduce((a, space, i) => {
-        const galaxiesIndex = findAllIndexes(space, (space) => space === '#');
-        const newPositions = galaxiesIndex.map((gi) => [i, gi]);
-        return newPositions.length > 0 ? a.concat(newPositions) : a;
-    }, []);
-    return galaxyPositions.reduce(
+console.log({ shortest });
+
+function findShortestDistancesBetweenGalaxies(pos) {
+    return pos.reduce(
         (a1, [gx, gy], i) =>
             a1 +
-            galaxyPositions
+            pos
                 .slice(0, i + 1)
                 .reduce((a2, [cx, cy]) => a2 + cdist(gx, cx, gy, cy), 0),
         0
     );
 }
 
+function getCalibratedPositions(galaxy) {
+    const galaxyPositions = galaxy.reduce((a, space, i) => {
+        const galaxiesIndex = findAllIndexes(space, (space) => space === '#');
+        const newPositions = galaxiesIndex.map((gi) => [i, gi]);
+        return newPositions.length > 0 ? a.concat(newPositions) : a;
+    }, []);
+
+    const { rowIndexes, colIndexes } = findRowColIndexes(galaxy);
+    const npos = [];
+
+    for (let i = 0; i < galaxyPositions.length; i++) {
+        const [gi, gj] = galaxyPositions[i];
+
+        const l_cols = colIndexes.filter((col) => col < gj).length * 999999;
+        const t_rows = rowIndexes.filter((row) => row < gi).length * 999999;
+        const new_j = gj + l_cols;
+        const new_i = gi + t_rows;
+
+        npos.push([new_i, new_j]);
+    }
+
+    return npos;
+}
+
 function cdist(x1, x2, y1, y2) {
     return Math.abs(x2 - x1) + Math.abs(y2 - y1);
 }
 
-function expandGalaxy(galaxy) {
+function findRowColIndexes(galaxy) {
     const rowIndexes = findAllIndexes(galaxy, checkSpaceForGalaxy);
     const colIndexes = findAllIndexes(
         rotateMatrix(galaxy),
         checkSpaceForGalaxy
     );
-    const galaxyForExp = [...galaxy];
-    expandGalaxyRows(galaxyForExp, rowIndexes);
-    const rotatedGalaxy = rotateMatrix(galaxyForExp);
-    expandGalaxyRows(rotatedGalaxy, colIndexes);
-    return rotateMatrix(rotatedGalaxy);
-}
-
-function expandGalaxyRows(galaxy, indexes) {
-    return indexes.forEach((ri, i) => {
-        galaxy.splice(ri + i, 0, galaxy[ri + i]);
-    });
+    return { rowIndexes, colIndexes };
 }
 
 function checkSpaceForGalaxy(row: string[], i = 0) {
@@ -53,5 +64,3 @@ function checkSpaceForGalaxy(row: string[], i = 0) {
     if (i === row.length) return i;
     return checkSpaceForGalaxy(row, i + 1);
 }
-
-console.log({ part1Answer: distances });
