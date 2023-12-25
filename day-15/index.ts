ea4e8f9356081693701681584ce36e169e046daf
Michał Walczuk <michal.walczuk@ext.denkwerk.com>
2023-12-25 02:43:29 +0100

    day 15: refactor slightly

--- day-15/index.ts
+++ day-15/index.ts
@@ -1,61 +1,33 @@
-const fs = require('fs');
+const { readFile } = require('../utils');
+const bxs = Array.from({ length: 256 }, () => []);
+const input = readFile('input.txt').split(',');
 
-const input = fs
-    .readFileSync('input.txt', 'utf-8')
-    .split(',')
-    .map((x) => x.replace('\n', ''));
+const hash = (s) => s.split('').reduce((a, ch) => ((ch.charCodeAt(0) + a) * 17) % 256, 0);
 
-const hash = (s: string) =>
-    s.split('').reduce((a, ch) => ((ch.charCodeAt(0) + a) * 17) % 256, 0);
+const lnData = (lens) =>
+    [lens.replace('-', '=').split('=')[0]].map((label) => ({ label, hash: hash(label) }))[0];
 
-const calcTotalPower = (lenses) => {
-    const boxes = [];
-    for (const lens of lenses) {
-        if (lens.includes('-')) {
-            const label = lens.split('-')[0];
-            const labelHash = hash(label);
-            const foundIndex = (boxes[labelHash] || []).findIndex(
-                (lkLens) => lkLens.split('=')[0] === label
-            );
-            if (foundIndex !== -1) {
-                boxes[labelHash].splice(foundIndex, 1);
-            }
-        } else if (lens.includes('=')) {
-            const label = lens.split('=')[0];
-            const labelHash = hash(label);
-            if (!boxes[labelHash] || boxes[labelHash].length === 0) {
-                boxes[labelHash] = [lens];
-            } else {
-                const foundIndex = boxes[labelHash].findIndex(
-                    (lkLens) => lkLens.split('=')[0] === label
-                );
-                if (foundIndex !== -1) {
-                    boxes[labelHash].splice(foundIndex, 1, lens);
-                } else {
-                    if (!boxes[labelHash]) {
-                        boxes[labelHash] = [lens];
-                    } else if (boxes[labelHash].length > 0) {
-                        boxes[labelHash].push(lens);
-                    }
-                }
-            }
-        }
+const solveLn = (ln) => {
+    const { label, hash } = lnData(ln);
+    if (ln.includes('-')) {
+        bxs[hash] = bxs[hash].filter((ln) => lnData(ln).label !== label);
     }
-    return boxes.reduce((acc, box, boxIndex) => {
-        return (
-            acc +
-            box.reduce((acc, lens, lensIndex) => {
-                return (
-                    acc + (boxIndex + 1) * (lensIndex + 1) * lens.split('=')[1]
-                );
-            }, 0)
-        );
-    }, 0);
+    if (ln.includes('=')) {
+        const i = bxs[hash].findIndex((ln) => lnData(ln).label === label);
+        if (i !== -1) bxs[hash][i] = i !== -1 ? ln : bxs[hash[i]];
+        else bxs[hash].push(ln);
+    }
+};
+
+const power = (lns) => {
+    lns.forEach(solveLn);
+    return bxs.reduce(
+        (a, ln, bi) => a + ln.reduce((a, ln, li) => (bi + 1) * (li + 1) * ln.split('=')[1] + a, 0),
+        0
+    );
 };
 
 console.log({
-    part1: input.reduce((acc, value) => {
-        return hash(value) + acc;
-    }, 0),
-    part2: calcTotalPower(input)
+    part1: input.reduce((acc, value) => hash(value) + acc, 0),
+    part2: power(input)
 });
